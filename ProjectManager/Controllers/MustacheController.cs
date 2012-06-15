206cc080590812b73aeee1a40616f495560e1187
Chris Worley <bluzeman@live.com>
2012-06-15 10:33:20 -0600

    added mustache

---
+++ ProjectManager/Controllers/MustacheController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManager.Controllers
+{
+    public class MustacheController : Controller
+    {
+        //
+        // GET: /Mustache/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
