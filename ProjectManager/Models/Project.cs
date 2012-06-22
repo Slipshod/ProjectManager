9dabbb8eac6c0b6fae5d21a471809fd94228a139
Chris Worley <bluzeman@live.com>
2012-06-21 19:42:32 -0600

    Final commit and merging with master

--- ProjectManager/Models/Project.cs
+++ ProjectManager/Models/Project.cs
@@ -14,7 +14,8 @@ namespace ProjectManager.Models
         public string Title { get; set; }
         public string Detail { get; set; }
         public bool Completed { get; set; }
-        public IEnumerable<SubTask> SubTasks { get; set; } 
+        public IEnumerable<SubTask> SubTasks { get; set; }
+        public string Temp { get; set; }
 
     }
 }
