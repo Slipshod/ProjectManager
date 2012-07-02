92245b9004fd9e5a9c5c27afd78ab32af05f9aaa
Chris Worley <bluzeman@live.com>
2012-07-02 15:00:14 -0600

    Committing for master merge

--- ProjectManager/Models/Project.cs
+++ ProjectManager/Models/Project.cs
@@ -20,18 +20,15 @@ namespace ProjectManager.Models
         public bool Completed { get; set; }
         public virtual IEnumerable<SubTask> SubTasks { get; set; }
 
-        public IList<Project> GetProjects(ProjectManagerDbContext _db)
+        public IList<Project> GetProjects(ProjectManagerDbContext db)
         {
-            var projects = _db.Projects.ToList();
+            var projects = db.Projects.ToList();
             foreach (var project in projects)
             {
                 var id = project.ProjectID;
-                project.SubTasks = _db.SubTasks.Where(t => t.ProjectID == id);
+                project.SubTasks = db.SubTasks.Where(t => t.ProjectID == id);
             }
-
             return projects;
         }
-
-
     }
 }
